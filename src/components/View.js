14e7027b3b328d091ff94018a605f27379ea1163
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-12-17 13:07:40 -0700

    working on view

--- src/components/View.js
+++ src/components/View.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
+import axiosWithAuth from '../utils/axiosWithAuth';
+import axios from 'axios';
 import Article from './Article';
 import EditForm from './EditForm';
 
@@ -8,6 +9,19 @@ const View = (props) => {
     const [articles, setArticles] = useState([]);
     const [editing, setEditing] = useState(false);
     const [editId, setEditId] = useState();
+
+    useEffect (()=> {
+        axiosWithAuth()
+            .get('/articles')
+                .then(res => {
+                    setArticles(res.data)
+
+                }).catch(err => {
+                    console.log(err)
+                })
+            }, [])
+            
+
 
     const handleDelete = (id) => {
     }
@@ -43,6 +57,7 @@ const View = (props) => {
         </ContentContainer>
     </ComponentContainer>);
 }
+
 
 export default View;
 
